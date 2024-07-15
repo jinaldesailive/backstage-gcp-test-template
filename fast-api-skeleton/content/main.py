5127d126dfe60876140d4430646e8f4716fb1435
Jinal Desai <58817990+jinaldesailive@users.noreply.github.com>
2024-07-15 16:18:41 +0100

    updated

--- fast-api-skeleton/content/main.py
+++ fast-api-skeleton/content/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 import uvicorn
+from sqlalchemy import MetaData, Table, create_engine, selectimport 
 
 app = FastAPI()
+engine = create_engine(
+    "spanner:///projects/png-gcp-learning-poc/instances/appdb/databases/tododb"
+)
+table = Table("tasks", MetaData(bind=engine), autoload=True)
 
 @app.get("/hc/")
 def healthcheck():
     return 'Health - OK'
+
+@app.get("/db/")
+def get_db_data():
+    with engine.begin() as connection:
+    for row in connection.execute(select(["*"], from_obj=table)).fetchall():
+        print(row)
 
 @app.get("/")
 async def root():
